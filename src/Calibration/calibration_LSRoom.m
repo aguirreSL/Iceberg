9b4a441785402b1e62516ccf299e29776b0fefad
aguirreSL <sergio@aguirre.eng.br>
2022-07-29 23:18:04 +0200

    Add line to install ita toolbox after git clone

--- src/Calibration/calibration_LSRoom.m
+++ src/Calibration/calibration_LSRoom.m
@@ -5,10 +5,16 @@ try
     clear test
 catch
     disp('Indeed we need ITA-Toolbox. We shall install it, dear')
-!git clone https://git.rwth-aachen.de/ita/toolbox.git Toolboxes/ITA-Toolbox
+!git clone https://git.rwth-aachen.de/ita/toolbox.git ../Toolboxes/ITA-Toolbox
+cd ..
+if ispc
+run([pwd '\Toolboxes\ITA-Toolbox\ita_toolbox_setup.m'])
+else
+    run([pwd '/Toolboxes/ITA-Toolbox/ita_toolbox_setup.m'])
 end
+cd Calibration
 ccx
-calibrationPath = pwd; 
+
 
 %% Create record session
 inputChannel    = 1;            %Set the channel that will receive the input 
